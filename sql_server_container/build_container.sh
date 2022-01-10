9de1b3a7cf2f6a2835e3a3849ab74ca9b1af2529
root <root@corbinblock>
2022-01-10 09:41:34 -0600

    initial

--- sql_server_container/build_container.sh
+++ sql_server_container/build_container.sh
@@ -2,6 +2,6 @@
 
 docker pull mcr.microsoft.com/mssql/server:2019-latest
 
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=<YourStrong@Passw0rd>" \
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=changeme" \
 	-p 1433:1433 --name sql1 -h sql1 \
 	-d mcr.microsoft.com/mssql/server:2019-latest
--- sql_server_container/run_container.ps1
+++
@@ -1,1 +0,0 @@
-docker container run -d -p 80:80 -t --name debian_dev_env debian_dev_env
