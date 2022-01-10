d768fd18cae62b96a26eb38678f5892e4546585a
root <root@corbinblock>
2022-01-10 09:07:09 -0600

    initial

--- jenkins_container/run_container.sh
+++ jenkins_container/run_container.sh
@@ -1,4 +1,4 @@
-docker run --name jenkins --rm --detach \
+docker run --name jenkins --restart=unless-stopped \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
   --publish 8080:8080 --publish 50000:50000 \
