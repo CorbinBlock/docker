7b591dedb0c1f26d0c65243bb422ccce2825ad9a
CorbinBlock <williamcorbinblock@gmail.com>
2022-01-12 00:31:17 +0000

    initial

--- debian_dev_env/run_container.ps1
+++ debian_dev_env/run_container.ps1
@@ -1,2 +1,2 @@
-docker run -d -p 80:80 -t --name debian_dev_env debian_dev_env
+docker run -d -p 5000:5000 -t --name debian_dev_env debian_dev_env
 # docker run -d -p 80:80 -t --name debian_dev_env debian_dev_env -m http.server --bind 0.0.0.0
